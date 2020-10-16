45a370d6e44ff01ba433c2fd622834e7313a33cd
Disha Shah <65646830+dishaShah01@users.noreply.github.com>
2020-10-16 18:32:35 +0530

    Update customer.java

--- ProjectFiles/customer.java
+++ ProjectFiles/customer.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.util.Vector;
 public class customer 
 {
 
@@ -15,7 +17,6 @@ public class customer
 	customer(String customerName,long customerID,long customerPhoneNo,long customerCredits,String customerAddress,String customerEmail,int customerSize,char customerGender,String customerPassword, String prevOrders)
 	{
 		this.customerID=customerID;
-		this.customerEmail=customerEmail;
 		this.customerName=customerName;
 		this.customerAddress=customerAddress;
 		this.customerCredits=customerCredits;
@@ -25,18 +26,31 @@ public class customer
 		this.customerPassword=customerPassword;
 		this.prevOrders=prevOrders;
 	}
-  
+	customer()
+	{
+		customerID=0;
+		customerEmail="";
+		customerName="";
+		customerAddress="";
+		customerCredits=0;
+		customerSize=0;
+		customerGender='\0';
+		customerPassword="";
+		prevOrders="";
+	}
 	
 	public static Vector<customer> cust=new Vector<customer>();
-
+	static Scanner s=new Scanner(System.in);
 	public static void main(String args[]) 
 	{
-		Scanner s=new Scanner(System.in);
+		
 
 		int op=0;
-		int n;
-		while(op!=6)
+		
+		
+		loop1: while(op!=3)
 		{
+			
 			System.out.println("MENU\n1)Sign Up\n2)Log In\n3)Exit\n");
 			op=s.nextInt();
 
@@ -47,7 +61,7 @@ public class customer
 				break;
 				case 2:
 				logIn();
-				break;
+				//break loop1;
 				case 3:
 				break;
 				default:
@@ -56,22 +70,21 @@ public class customer
 		}
 	}
 
-	public static signUp()
+	public static void signUp()
 	{
-		Scanner s=new Scanner(System.in);
+		
 		System.out.println("Enter Your Details:\n");
 		
-		System.out.println("Email ID:\n");
-		String email=s.nextLine();
+		System.out.println("Email ID:");
+		String email=s.next();
 
-		boolean accountExists=check(email,0);
+		boolean accountExists=check(email,"");
 		int op=0;
 		if(accountExists)
 		{
-			while(op!=1 || op!=2)
-			{
+			
 				System.out.println("You already have an account with this email Id\n1)Log in?\n2)Create new account with different Email ID\n");
-				op=nextInt();
+				op=s.nextInt();
 				switch(op)
 				{
 					case 1:logIn();
@@ -81,7 +94,7 @@ public class customer
 					default:
 					System.out.println("Please enter a valid option");
 				}
-			}		
+					
 		}
 		else
 		{
@@ -90,18 +103,18 @@ public class customer
 	}
 	public static void create(String email)
 	{
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter Your Details:\n");
-		System.out.println("Name:\n");
-		String name=s.nextLine();
-		System.out.println("Address:\n");
-		String add=s.nextLine();
-		System.out.println("Phone Number:\n");
+		
+		
+		System.out.println("Name:");
+		String name=s.next();
+		System.out.println("Address:");
+		String add=s.next();
+		System.out.println("Phone Number:");
 		long phone=s.nextLong();
-		System.out.println("Password:\n");
-		String pass=s.nextLine();
+		System.out.println("Password:");
+		String pass=s.next();
 		customer c=new customer(name,0,phone,0,add,email,0,'N',pass, "No Orders yet");
-		cust.(c);
+		cust.add(c);
 		System.out.println("Account created successfully\n");//go to store menu
 	}
 
@@ -109,10 +122,10 @@ public class customer
 	{
 		int op=0;
 		System.out.println("Enter the following details:\n");
-		System.out.println("Email ID:\n");
-		String email=s.nextLine();
-		System.out.println("Password:\n");
-		String pass=s.nextLine();
+		System.out.println("Email ID:");
+		String email=s.next();
+		System.out.println("Password:");
+		String pass=s.next();
 		boolean validInfo=check(email,pass);
 		if(validInfo)
 		{
@@ -125,7 +138,7 @@ public class customer
 			while(op!=1 || op!=2)
 			{
 				System.out.println("Wrong email ID/Password\n1)Try Again\n2)Sign Up\n");
-				op=nextInt();
+				op=s.nextInt();
 				switch(op)
 				{
 					case 1:logIn();
@@ -138,5 +151,45 @@ public class customer
 				}
 			}
 		}
+	}
+
+	public static boolean check(String e,String n)
+	{
+		boolean b;
+		b=true;
+		if(cust.size()==0)
+		b=false;
+		for(int i=0;i<cust.size();i++)
+		{
+			
+			
+			if(e.equals(cust.get(i).customerEmail))
+			{			
+				
+				if(n!="")
+				{
+					if(n.equals(cust.get(i).customerPassword))
+					{
+						b=true;						
+						return b;
+					}
+					else
+					{
+						b=false;						
+					}					
+				}
+				else
+				{
+					b=true;					
+					return b;
+				}
+			}
+			else
+			{
+				b=false;				
+			}
+		}
+		
+		return b;
 	}
 }
