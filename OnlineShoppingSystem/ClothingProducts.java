3b20d40208d05469ef48e0c6f8778304ede37516
Nidhi Bhanushali <71250816+NidhiBhanushali3701@users.noreply.github.com>
2020-10-17 11:23:10 +0530

    package OnlineShoppingSystem

---
+++ OnlineShoppingSystem/ClothingProducts.java
@@ -0,0 +1,19 @@
+public class ClothingProducts extends product
+{   
+    public ClothingProducts()
+    {
+        super();
+
+        System.out.println("Clothing Product Class");
+    }
+
+    public ClothingProducts(long productId,long productCost,String productName,long productBuyerID,String productBuyerName,long productSellerID,String productSellerName,String productDescription)
+    {
+        super(productId,productCost,productName,productBuyerID,productBuyerName,productSellerID,productSellerName,productDescription);
+    }
+
+    public static void main(String args[]) 
+    {
+
+    }
+}
---
+++ OnlineShoppingSystem/customer.java
@@ -0,0 +1,47 @@
+public class customer 
+{
+	String customerName;
+	long customerPhoneNo;
+	String customerEmail;
+	String customerAddress;
+	long customerCredits;
+	long customerID;
+	int customerSize;
+  	char customerGender;
+	String customerPassword;
+	String prevOrders;
+
+	customer(String customerName,long customerID,long customerPhoneNo,long customerCredits,String customerAddress,String customerEmail,int customerSize,char customerGender,String customerPassword, String prevOrders)
+	{
+		this.customerID=customerID;
+		this.customerEmail=customerEmail;
+		this.customerName=customerName;
+		this.customerAddress=customerAddress;
+		this.customerCredits=customerCredits;
+		this.customerEmail=customerEmail;
+		this.customerSize=customerSize;
+		this.customerGender=customerGender;
+		this.customerPassword=customerPassword;
+		this.prevOrders=prevOrders;
+	}
+  
+	customer()
+	{
+		customerAddress="";
+		customerEmail="";
+		customerName="";
+		customerCredits=0;
+		customerPhoneNo=0;
+		customerID=0;
+		customerSize=0;
+		customerGender='\0';
+		customerPassword="";
+		prevOrders="";
+		
+	}
+
+	public static void main(String args[]) 
+	{
+		System.out.println("customer");
+	}
+}
