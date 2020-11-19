fe26b27ceecb81949762ae5ef40deb9f121b07cf
Nidhi Bhanushali <71250816+NidhiBhanushali3701@users.noreply.github.com>
2020-11-19 19:18:52 +0530

    updated

--- OnlineShoppingSystem/deliveredThread.java
+++ OnlineShoppingSystem/deliveredThread.java
@@ -4,6 +4,24 @@ class deliveredThread extends Thread
 {
     public void run()
     {
-        
+        try
+        {
+            Thread.sleep(5000);
+            System.out.println("\t\t\tSTARTED PACKING YOUR PRODUCTS(S) :)");
+            Thread.sleep(7500);
+            System.out.println("\t\t\tSHIPPED :)");
+            Thread.sleep(10000);
+            System.out.println("\t\t\tOUT FOR DELIVERY :)");
+            Thread.sleep(15000);
+            System.out.println("\t\t\tPRODUCT DELIVERED :)");
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        finally
+        {
+            System.out.println("\t\t\t HAPPY SHOPPING ;-)");
+        }
     }
 }
