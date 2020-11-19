4c417e0a097e09d794206c33b88d6a6baf1ef015
Disha Shah <65646830+dishaShah01@users.noreply.github.com>
2020-11-19 21:19:19 +0530

    Update accountSettings.java

--- OnlineShoppingSystem/accountSettings.java
+++ OnlineShoppingSystem/accountSettings.java
@@ -6,17 +6,15 @@ import java.util.regex.Matcher;
 
 public class accountSettings extends customerDashBoard{
     
-    public accountSettings(customer thisCustomer) {
-        super(thisCustomer);
+    public accountSettings(customer thisCustomer, Vector cust) {
+        super(thisCustomer,cust);
     }
-    public accountSettings() {
-        super();
-    }
+
 	public void settingsmenu(customer thisCustomer) {
         Scanner s=new Scanner(System.in);
         int op;
-        
-            System.out.println("\n\t1) DISPLAY DETAILS\n\t2) EDIT DETAILS\n\t3) SIGN OUT\n\t4) DELETE ACCOUNT\n\t5) BACK\n\t0) EXIT\n");
+        //System.out.println(cust.get(1).customerName);
+            System.out.println("\n1)DISPLAY DETAILS\n2)EDIT DETAILS\n3)SIGN OUT\n4)DELETE ACCOUNT\n5)BACK\n6)EXIT\n");
             op=s.nextInt();
             switch(op)
             {
@@ -29,6 +27,7 @@ public class accountSettings extends customerDashBoard{
                 settingsmenu(thisCustomer);
                 break;
                 case 3:
+                
                 customMenu();
                 settingsmenu(thisCustomer);
                 break;
@@ -39,11 +38,11 @@ public class accountSettings extends customerDashBoard{
                 displayCustomerDashBoard(thisCustomer);
                 settingsmenu(thisCustomer);
                 break;
-                case 0:
+                case 6:
                 System.exit(0);
                 break;
                 default:
-                System.out.println("INVALID CHOICE, Try Again.");
+                System.out.println("Wrong input, try again.");
             }
         
         
@@ -52,10 +51,12 @@ public class accountSettings extends customerDashBoard{
 
     public void display(customer thisCustomer)
     {
-        System.out.println( "\tName   "+thisCustomer.customerName);
-        System.out.println( "\tEmail ID   "+thisCustomer.customerEmail);
-        System.out.println( "\tPhone Number   "+ thisCustomer.customerPhoneNo);
-        System.out.println( "\tAddress   "+thisCustomer.customerAddress);
+        
+        System.out.println( "Name "+thisCustomer.customerName);
+        System.out.println( "Email ID "+thisCustomer.customerEmail);
+        System.out.println( "Phone Number "+ thisCustomer.customerPhoneNo);
+        System.out.println( "Address "+thisCustomer.customerAddress);
+        
     }
     
     public void edit(customer thisCustomer)
@@ -68,48 +69,46 @@ public class accountSettings extends customerDashBoard{
         String newpass="";
         String regexStr = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"; 
 		Pattern pattern = Pattern.compile(regexStr); 
-        System.out.println("\nEDIT:\n1) Name\n2) Addrress\n3) Phone Number\n4) Email ID\n5) Password\n0) BACK");
+        System.out.println("\nEDIT:\n1)Name\n2)Addrress\n3)Phone Number\n4)Email ID\n5)Password");
         o=s.nextInt();
         s.nextLine();
         boolean validity=false;
         switch(o)
         {
             case 1:
-                System.out.println( " Name\t"+thisCustomer.customerName);
-                System.out.print( " New Name\t");
-                name=s.nextLine();
-                thisCustomer.customerName=name;
+            System.out.println( "Name "+thisCustomer.customerName);
+            System.out.println( "New Name ");
+            name=s.nextLine();
+            thisCustomer.customerName=name;
             break;
             case 2:
-                System.out.println( " Address\t"+thisCustomer.customerAddress);
-                System.out.print( " New Address\t");
-                add=s.nextLine();
-                thisCustomer.customerAddress=add;
+            System.out.println( "Address "+thisCustomer.customerAddress);
+            System.out.println( "New Address ");
+            add=s.nextLine();
+            thisCustomer.customerAddress=add;
             break;
             case 3:
-                System.out.println( " Phone Number\t"+thisCustomer.customerPhoneNo);
-                while(!validity)
-                {
-                    try
-                    {
-                        System.out.print( " New Phone number\t");
-                        p=s.nextLong();
-                        if(p<1000000000)
-                            throw new phonenumberException("Please enter a valid phone number(10 digits)");
-                        else validity=true;
-                    }
-                    catch(phonenumberException e)
-                    {
-                        System.out.println(e);
-                    }
-                }
-                thisCustomer.customerPhoneNo=p;
+            System.out.println( "Phone Number "+thisCustomer.customerPhoneNo);
+            while(!validity){
+            try{
+            System.out.println( "New Phone number ");
+            p=s.nextLong();
+            if(p<1000000000)
+                throw new phonenumberException("Please enter a valid phone number(10 digits)");
+		    else validity=true;
+            }
+            catch(phonenumberException e)
+            {
+                System.out.println(e);
+            }
+            }
+            thisCustomer.customerPhoneNo=p;
             break;
             case 4:
-                System.out.println( "Email ID "+thisCustomer.customerEmail);
-                System.out.println( "New ID ");
-                id=s.nextLine();
-                thisCustomer.customerEmail=id;
+            System.out.println( "Email ID "+thisCustomer.customerEmail);
+            System.out.println( "New ID ");
+            id=s.nextLine();
+            thisCustomer.customerEmail=id;
             break;
             case 5:
             System.out.println("Enter your old password");
@@ -140,11 +139,8 @@ public class accountSettings extends customerDashBoard{
                 edit(thisCustomer);
             }
             break;
-            case 0:
-            System.out.println("GOING BACK");
-            break;
             default:
-            System.out.println("Invalid choice".toUpperCase());
+            System.out.println("Invalid choice");
             edit(thisCustomer);
             break;
 
